219bb6227249644c2cfb22310bca79d7af136730
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2022-02-10 21:20:45 +0300

    Show active passcards

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -6,8 +6,11 @@ from django.shortcuts import render
 def active_passcards_view(request):
     # Программируем здесь
 
-    all_passcards = Passcard.objects.all()
+    all_passcards = Passcard.objects.filter(is_active=True)
+
     context = {
         'active_passcards': all_passcards,  # люди с активными пропусками
     }
     return render(request, 'active_passcards.html', context)
+
+
