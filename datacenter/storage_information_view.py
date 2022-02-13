832db0facd4a4e83edae53b8435a8a13ffa18fe8
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2022-02-13 19:41:54 +0300

    Show person who at the storage

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -5,14 +5,16 @@ from django.shortcuts import render
 
 def storage_information_view(request):
     # Программируем здесь
-
+    visits = Visit.objects.filter(leaved_at__isnull=True)
     non_closed_visits = [
         {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
+            'who_entered': visit.passcard,
+            'entered_at': visit.entered_at,
+            'duration': '25:03'
+
         }
-    ]
+        for visit in visits]
+
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
