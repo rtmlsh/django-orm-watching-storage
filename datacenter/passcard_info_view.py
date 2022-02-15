7a4d9790fa7ec83bbb6c4cdf0aada90cd050e290
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2022-02-16 00:21:12 +0300

    Show data about timing for each person

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,21 +1,25 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from datacenter.models import get_duration, format_duration, is_visit_long
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
-
-    this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
-    ]
+    owner = Passcard.objects.get(passcode=passcode)
+    visits = Visit.objects.filter(passcard=owner)
+    this_passcard_visits = []
+    for visit in visits:
+        duration = get_duration(visit)
+        spent_time = format_duration(duration)
+        answer = is_visit_long(spent_time)
+        personal_data = {
+            'entered_at': visit.entered_at,
+            'duration': spent_time,
+            'is_strange': answer
+        }
+        this_passcard_visits.append(personal_data)
     context = {
-        'passcard': passcard,
+        'passcard': owner,
         'this_passcard_visits': this_passcard_visits
     }
     return render(request, 'passcard_info.html', context)
