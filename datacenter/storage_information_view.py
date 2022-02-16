70ec85261dbb088ef34a6e29fadfd89426d424fe
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2022-02-16 21:40:50 +0300

    Check the code

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,24 +1,22 @@
-from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
-from datacenter.models import get_duration, format_duration
+from datacenter.models import get_duration, format_duration, is_visit_long
 
 
 def storage_information_view(request):
     visits = Visit.objects.filter(leaved_at__isnull=True)
     non_closed_visits = []
-
     for visit in visits:
         duration = get_duration(visit)
         spent_time = format_duration(duration)
+        answer = is_visit_long(spent_time)
         personal_data = {
             'who_entered': visit.passcard,
             'entered_at': visit.entered_at,
-            'duration': spent_time
-
+            'duration': spent_time,
+            'is_strange': answer
         }
         non_closed_visits.append(personal_data)
-
     context = {
         'non_closed_visits': non_closed_visits,
     }
