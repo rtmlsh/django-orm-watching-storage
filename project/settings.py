1c9297f56aade3170f70a9253730616d650d5ba5
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2022-02-21 22:48:40 +0300

    Short environment variables

--- project/settings.py
+++ project/settings.py
@@ -7,16 +7,7 @@ load_dotenv()
 env = Env()
 env.read_env()
 
-DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-    }
-}
+DATABASES = {"default": env.dj_db_url("DATABASE_URL")}
 
 INSTALLED_APPS = ['datacenter']
 
